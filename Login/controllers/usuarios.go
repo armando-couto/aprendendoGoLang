01a859957d35a44bf3ca79d8da7d79b4a3bb2b44
Lucas Macedo <103705571+LucasMacedoViana@users.noreply.github.com>
2022-08-18 16:04:32 -0300

    Add files via upload

---
+++ Login/controllers/usuarios.go
@@ -0,0 +1,72 @@
+package controllers
+
+import (
+	"awesomeProject/Login/models"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsUsuarios := models.BuscaTodosOsUsuarios()
+	temp.ExecuteTemplate(w, "Index", todosOsUsuarios)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		email := r.FormValue("email")
+		squad := r.FormValue("squad")
+		permissao := r.FormValue("permissao")
+
+		squadConvertidaParaInt, err := strconv.Atoi(squad)
+		if err != nil {
+			log.Println("Erro na conversão do squad:", err)
+		}
+
+		models.CriaNovoUsuario(nome, email, squadConvertidaParaInt, permissao)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idDoUsuario := r.URL.Query().Get("id")
+	models.DeletaUsuario(idDoUsuario)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idDoUsuario := r.URL.Query().Get("id")
+	usuario := models.EditaUsuario(idDoUsuario)
+	temp.ExecuteTemplate(w, "Edit", usuario)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		email := r.FormValue("email")
+		squad := r.FormValue("squad")
+		permissao := r.FormValue("permissao")
+
+		idConvertidaParaInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na convesão do ID para int:", err)
+		}
+
+		squadConvertidaParaInt, err := strconv.Atoi(squad)
+		if err != nil {
+			log.Println("Erro na convesão da squad para int:", err)
+		}
+
+		models.AtualizaUsuario(idConvertidaParaInt, nome, email, squadConvertidaParaInt, permissao)
+	}
+	http.Redirect(w, r, "/", 301)
+}
