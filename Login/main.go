4a822c9113f275263db43c37dd8c62ea62d7cb68
Lucas Macedo <lucasmacedo9@hotmail.com>
2022-09-15 14:04:40 -0300

    API de login com jwt

--- Login/main.go
+++ Login/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	"mesa/routes"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"log"
+	"login/controllers"
+	"login/database"
+	"login/middlewares"
+	"login/models"
 )
 
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	api := r.Group("/api")
+	{
+		public := api.Group("/public")
+		{
+			public.POST("/login", controllers.Login)
+			public.POST("/signup", controllers.Signup)
+		}
+
+		// here
+		protected := api.Group("/protected").Use(middlewares.Authz())
+		{
+			protected.GET("/profile", controllers.Profile)
+		}
+	}
+
+	return r
+}
 func main() {
-	routes.CarregaRotas()
-	http.ListenAndServe(":8000", nil)
+	err := database.InitDatabase()
+	if err != nil {
+		log.Fatalln("could not create database", err)
+	}
+
+	database.GlobalDB.AutoMigrate(&models.User{})
+
+	r := setupRouter()
+	r.Run(":8080")
 }
---
+++ Login/models/models.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"login/database"
+)
+
+// User define o usuario na base de dados
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
+}
+
+// CreateUserRecord Cria um registro de usuario na base de dados
+func (user *User) CreateUserRecord() error {
+	result := database.GlobalDB.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+// HashPassword encripta a senha do usuario
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(bytes)
+
+	return nil
+}
+
+// CheckPassword checa a senha do usuario
+func (user *User) CheckPassword(providedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
